6de13a39cb3b90a5de59d1a3f034402418d32db1
Morgan Bligh <morganbligh@icloud.com>
2021-05-15 20:16:27 -0700

    HW 4 MODELS

--- pyth0nclub/club/models.py
+++ pyth0nclub/club/models.py
@@ -1,3 +1,54 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Meeting(models.Model):
+    meetingtitle=models.CharField(max_length=255)
+    meetingdate=models.DateField()
+    meetingtime=models.DateTimeField()
+    location=models.CharField(max_length=255)
+    agenda=models.TextField()
+
+    def __str__(self):
+        return self.meetingtitle
+    
+    class Meta:
+        db_table='meeting'
+
+class MeetingMinute(models.Model):
+    meetingid=models.ForeignKey(Meeting, on_delete=models.CASCADE)
+    meetingattendance=models.ManyToManyField(User)
+    meetingminutes=models.TextField()
+
+    def __str__(self):
+        return self.meetingid
+
+    class Meta:
+        db_table='meetingminute'
+
+class Resource(models.Model):
+    resourcename=models.CharField(max_length=255)
+    resourcetype=models.CharField(max_length=255)
+    url=models.URLField()
+    dateentered=models.DateField()
+    userid=models.ForeignKey(User, on_delete=models.CASCADE)
+    description=models.TextField()
+
+    def __str__(self):
+        return self.resourcename
+
+    class Meta:
+        db_table='resource'
+
+class Event(models.Model):
+    title=models.CharField(max_length=255)
+    location=models.CharField(max_length=255)
+    date=models.DateField()
+    time=models.DateTimeField()
+    description=models.TextField()
+    userid=models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table='event'
