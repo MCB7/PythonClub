b691ed2675f067815889c403138d7f968eb5b954
Morgan Bligh <morganbligh@icloud.com>
2021-05-16 17:01:57 -0700

    HW 5

--- pyth0nclub/club/views.py
+++ pyth0nclub/club/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Resource, Meeting, Event
 
 # Create your views here.
 def index(request): 
     return render(request, 'club/index.html')
+
+def resource(request):
+    resource_list=Resource.objects.all()
+    return render(request, 'club/resources.html',{'resource_list': resource_list})
     
