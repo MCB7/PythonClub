430c555d6fa6b52eae76eb5ed01e2b7a3bd3e5c7
Morgan Bligh <morganbligh@icloud.com>
2021-05-28 17:22:50 -0700

    HW 6

--- pyth0nclub/club/views.py
+++ pyth0nclub/club/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Resource, Meeting, Event
 
 # Create your views here.
@@ -8,4 +8,13 @@ def index(request):
 def resource(request):
     resource_list=Resource.objects.all()
     return render(request, 'club/resources.html',{'resource_list': resource_list})
+
+def meeting(request):
+    meeting_list=Meeting.objects.all()
+    return render(request, 'club/meeting.html',{'meeting_list': meeting_list})
+
+def meetingdetail(request, id):
+    meeting=get_object_or_404(Meeting, pk=id)
+    return render(request, 'club/meetingdetail.html', {'detailmeeting' : meeting})
+
     
