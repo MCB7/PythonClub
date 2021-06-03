b02c5ba8dd770c38622238358fef84447021a339
Morgan Bligh <morganbligh@icloud.com>
2021-06-02 17:43:00 -0700

    HW 8

--- pyth0nclub/club/views.py
+++ pyth0nclub/club/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Resource, Meeting, Event
+from .forms import ResourceForm
+from .forms import MeetingForm
 
 # Create your views here.
 def index(request): 
@@ -17,4 +19,28 @@ def meetingdetail(request, id):
     meeting=get_object_or_404(Meeting, pk=id)
     return render(request, 'club/meetingdetail.html', {'detailmeeting' : meeting})
 
-    
+def newResource(request):
+    form=ResourceForm
+
+    if request.method=='POST':
+        form=ResourceForm(request.POST)
+        if form.is_valid():
+            post=form.save(commit=True)
+            post.save()
+            form=ResourceForm()
+    else:
+        form=ResourceForm()
+    return render(request, 'club/newresource.html', {'form': form})
+
+def newMeeting(request):
+    form=MeetingForm
+
+    if request.method=='POST':
+        form=MeetingForm(request.POST)
+        if form.is_valid():
+            post=form.save(commit=True)
+            post.save()
+            form=MeetingForm()
+    else:
+        form=MeetingForm()
+    return render(request, 'club/newresource.html', {'form': form})
